6ece64e8092f99d9ff87f60077a9160969edb390
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-12 17:57:54 -0300

    Criar método para exibir compras

--- src/main/java/br/com/zup/Ecommerce/controllers/CompraController.java
+++ src/main/java/br/com/zup/Ecommerce/controllers/CompraController.java
@@ -3,9 +3,12 @@ package br.com.zup.Ecommerce.controllers;
 import br.com.zup.Ecommerce.dtos.CompraDTO;
 import br.com.zup.Ecommerce.services.CompraService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CompraController {
@@ -15,6 +18,11 @@ public class CompraController {
     @PostMapping("/compras")
     public void efetuarCompra(@RequestBody CompraDTO compraDto){
         compraService.efetuarCompra(compraDto);
+    }
+
+    @GetMapping("/compras")
+    public List<CompraDTO> exibirCompras(){
+        return compraService.getCompras();
     }
 
 }
