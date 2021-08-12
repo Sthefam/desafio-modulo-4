92543cd9774099cce97cfbb0492bde985f91f7f9
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-12 14:15:54 -0300

    Criar método para adicionar o cliente na lista de clientes

--- src/main/java/br/com/zup/Ecommerce/services/ClienteService.java
+++ src/main/java/br/com/zup/Ecommerce/services/ClienteService.java
@@ -1,2 +1,27 @@
-package br.com.zup.Ecommerce.services;public class ClienteService {
+package br.com.zup.Ecommerce.services;
+
+import br.com.zup.Ecommerce.dtos.ClienteDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    private List<ClienteDTO> clientes = new ArrayList<>();
+
+    public void verificaDuplicidade(ClienteDTO clienteDto){
+        for(ClienteDTO cliente : clientes){
+            if(cliente.getCpf().equals(clienteDto.getCpf()) || cliente.getEmail().equals(clienteDto.getEmail())){
+                throw new RuntimeException("Cliente já cadastrado!");
+            }
+        }
+    }
+
+    public void cadastrarCliente(ClienteDTO clienteDto){
+        verificaDuplicidade(clienteDto);
+        clientes.add(clienteDto);
+    }
+
 }
