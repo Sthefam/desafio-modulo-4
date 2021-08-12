325b3d6dddf4e3013069257686aa0a1db1a4c633
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-12 14:35:54 -0300

    Criar método para pesquisar cliente pelo cpf

--- src/main/java/br/com/zup/Ecommerce/controllers/ClienteController.java
+++ src/main/java/br/com/zup/Ecommerce/controllers/ClienteController.java
@@ -1,2 +1,24 @@
-package br.com.zup.Ecommerce.controllers;public class ClienteController {
+package br.com.zup.Ecommerce.controllers;
+
+import br.com.zup.Ecommerce.dtos.ClienteDTO;
+import br.com.zup.Ecommerce.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping
+    public void cadastrarCliente(@RequestBody ClienteDTO clienteDto){
+        clienteService.cadastrarCliente(clienteDto);
+    }
+
+    @GetMapping("cliente/{cpf}")
+    public ClienteDTO pesquisarCliente(@PathVariable String cpf){
+        return clienteService.pesquisarCliente(cpf);
+    }
+
 }
