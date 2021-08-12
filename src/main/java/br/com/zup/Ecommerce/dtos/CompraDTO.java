9cec64455aeaa9ac38079ecf3bf28527ba667712
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-12 13:53:47 -0300

    Criar método para adicionar produto na lista de produtos

--- src/main/java/br/com/zup/Ecommerce/dtos/CompraDTO.java
+++ src/main/java/br/com/zup/Ecommerce/dtos/CompraDTO.java
@@ -1,2 +1,28 @@
-package br.com.zup.Ecommerce.dtos;public class CompraDTO {
+package br.com.zup.Ecommerce.dtos;
+
+import java.util.List;
+
+public class CompraDTO {
+
+    private ClienteDTO cliente;
+    private List<ProdutoDTO> produtos;
+
+    public CompraDTO() {
+    }
+
+    public ClienteDTO getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(ClienteDTO cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<ProdutoDTO> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<ProdutoDTO> produtos) {
+        this.produtos = produtos;
+    }
 }
--- src/main/java/br/com/zup/Ecommerce/services/ProdutoService.java
+++ src/main/java/br/com/zup/Ecommerce/services/ProdutoService.java
@@ -1,2 +1,26 @@
-package br.com.zup.Ecommerce.services;public class ProdutoService {
+package br.com.zup.Ecommerce.services;
+
+import br.com.zup.Ecommerce.dtos.ProdutoDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    private List<ProdutoDTO> produtos;
+
+    public void verificaDuplicidade(ProdutoDTO produtoDto){
+        for(ProdutoDTO produto : produtos){
+           if(produto.getNome().equals(produtoDto.getNome())){
+               throw new RuntimeException("Produto já cadastrado!");
+           }
+        }
+    }
+
+    public void cadastrarProduto(ProdutoDTO produtoDto){
+        verificaDuplicidade(produtoDto);
+        produtos.add(produtoDto);
+    }
+
 }
